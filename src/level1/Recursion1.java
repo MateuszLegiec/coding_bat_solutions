2a9093695975c91163f26a9b1c4d9cad634b03c2
MateuszLegiec <37617028+MateuszLegiec@users.noreply.github.com>
2018-05-26 00:26:52 +0200

    Update Recursion1.java

--- src/level1/Recursion1.java
+++ src/level1/Recursion1.java
@@ -5,19 +5,19 @@ package level1;
  * @author MateuszLegiec
  */
 public class Recursion1 {
-      /*
- *
- * Given n of 1 or more, return the factorial of n, which is n * (n-1) *
- * (n-2) ... 1. Compute the result recursively (without loops).
- */
+	 /*
+	 *
+	 * Given n of 1 or more, return the factorial of n, which is n * (n-1) *
+	 * (n-2) ... 1. Compute the result recursively (without loops).
+	 */
 public int factorial(int n) {
   if(n==1) 
       return 1;
   return n*factorial(n-1);
 }
 
-    /*
-     * We have a number of bunnies and each bunny has two big floppy ears. We
+    	/*
+         * We have a number of bunnies and each bunny has two big floppy ears. We
 	 * want to compute the total number of ears across all the bunnies
 	 * recursively (without loops or multiplication).
 	 */
@@ -25,10 +25,8 @@ public int bunnyEars(int bunnies) {
   if(bunnies==0)  return 0;
   return 2+bunnyEars(bunnies-1);
 }
-
-
-    /*
-     * The fibonacci sequence is a famous bit of mathematics, and it happens to
+        /*
+    	 * The fibonacci sequence is a famous bit of mathematics, and it happens to
 	 * have a recursive definition. The first two values in the sequence are 0
 	 * and 1 (essentially 2 base cases). Each subsequent value is the sum of the
 	 * previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13,
@@ -42,8 +40,8 @@ public int fibonacci(int n) {
       return 1;
   return fibonacci(n-1)+fibonacci(n-2);
 }
-    /*
-     * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+        /*
+         * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
 	 * (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 	 * have 3 ears, because they each have a raised foot. Recursively return the
 	 * number of "ears" in the bunny line 1, 2, ... n (without loops or
@@ -56,7 +54,7 @@ public int bunnyEars2(int bunnies) {
       return 2+bunnyEars2(bunnies-1);
   return 3+bunnyEars2(bunnies-1);
 }
-    /*
+        /*
 	 * We have triangle made of blocks. The topmost row has 1 block, the next
 	 * row down has 2 blocks, the next row has 3 blocks, and so on. Compute
 	 * recursively (no loops or multiplication) the total number of blocks in
@@ -68,7 +66,7 @@ public int triangle(int rows) {
   return  rows+triangle(rows-1);
 }
 
-    /*
+        /*
 	 * Given a non-negative int n, return the sum of its digits recursively (no
 	 * loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is
 	 * 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
@@ -79,7 +77,7 @@ public int sumDigits(int n) {
   return n%10 + sumDigits(n/10);
 }
 
-    /*
+        /*
 	 * Given a non-negative int n, return the count of the occurrences of 7 as a
 	 * digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10
 	 * yields the rightmost digit (126 % 10 is 6), while divide (/) by 10
@@ -92,8 +90,7 @@ public int count7(int n) {
       return 1+count7(n/10);
   return count7(n/10);
 }
-    /*
-
+      /*
        * Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit,
        * except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4.
        * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
@@ -109,7 +106,7 @@ public int count8(int n) {
   return count8(n/10);
 }
 
-    /*
+        /*
 	 * Given base and n that are both 1 or more, compute recursively (no loops)
 	 * the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 	 */
@@ -118,7 +115,7 @@ public int powerN(int base, int n) {
       return base;
   return base*powerN(base,n-1);
 }
-
+ 
     /*
      * Given a string, compute recursively (no loops) the number of lowercase
      * 'x' chars in the string.
@@ -130,6 +127,7 @@ public int countX(String str) {
       return 1+countX(str.substring(1));
   return countX(str.substring(1));
 }
+	
     /*
      * Given a string, compute recursively (no loops) the number of times
      * lowercase "hi" appears in the string.
@@ -141,8 +139,7 @@ public int countHi(String str) {
       return 1+countHi(str.substring(1));
   return countHi(str.substring(1));
 }
-
-
+	
     /*
      * Given a string, compute recursively (no loops) a new string where all the
      * lowercase 'x' chars have been changed to 'y' chars.
@@ -154,9 +151,7 @@ public String changeXY(String str) {
       return "y"+changeXY(str.substring(1));
   return str.charAt(0)+changeXY(str.substring(1));
 }
-
-
-    /*
+        /*
 	 * Given a string, compute recursively (no loops) a new string where all
 	 * appearances of "pi" have been replaced by "3.14".
 	 */
